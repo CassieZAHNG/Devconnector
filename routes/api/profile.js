707b57f5b5a29814e4f3dd8f7187eef78c7d93b2
Cassie <cassiebetter@gmail.com>
2019-11-12 15:01:07 +1100

    get all profiles & profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,11 +108,43 @@ router.post(
       console.error(err.message);
       res, status(500).send('Server Error');
     }
-
-    console.log(profileFields.skills);
-
-    res.send('hello');
   }
 );
+
+//@route  GET api/profile
+//@desc   Get all profiles
+//@access Public
+//get all profiles by profiles id
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@route  GET api/profile/user/:user_id
+//@desc   Get profile by user ID
+//@access Public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
