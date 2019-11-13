c4db6c15599ab703526febf1d1450f7ac6eafb5e
Cassie <cassiebetter@gmail.com>
2019-11-14 10:51:31 +1100

    create profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,3 +19,45 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+//Create or update profile
+//history: push() redirect after submit the form
+//edit: if updating|editting|creating
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+    //if edit is true, then say profile update; else say profile created
+    dispatch(setAlert(edit ? "Profile Update" : "Profile Created", "success"));
+
+    //if editing it, stay on the page, if creating it, then redirect
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
